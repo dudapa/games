a3a22a8a7ee1e40babec1e2dab4b25ccc2fcd676
Patrik Duda <patrikduda94@gmail.com>
2022-01-07 20:19:41 +0100

    Snake is going by itself

--- snake/script.js
+++ snake/script.js
@@ -1,17 +1,14 @@
 // listener
 document.addEventListener('keydown', playerMove);
 
-
 // canvas and context
 const canvas = document.querySelector('.myCanvas');
 const ctx = canvas.getContext('2d');
-
 
 // playboard
 const width = (canvas.width = 600);
 const height = (canvas.height = 600);
 const radius = 25;
-
 
 // player
 const initialPositionX = width / 2 - radius;
@@ -19,51 +16,52 @@ const initialPositionY = height / 2 + radius;
 let playerPositionX = initialPositionX;
 let playerPositionY = initialPositionY;
 
+// motion variables
+let velocityX = 1;
+let velocityY = 0;
+let snakeSpeed = 2 * radius;
+const fps = 10;
 
-// move 
-velocityX = 1
-velocityY = 0
-
-
-// grid
+// initial position
 drawGrid(radius, width, height);
 playerPosition(playerPositionX, playerPositionY);
 
+// game loop function (main function)
+function gameLoop() {
+  drawGrid(radius, width, height);
+  playerPosition(playerPositionX, playerPositionY);
+  snakeMotion();
+  checkOutPosition();
+  setTimeout(gameLoop, 1000 / fps);
+}
+
+// gameLoop()
 
 // playerMove function
 function playerMove(event) {
   if (event.key === 'ArrowUp' || event.key === 'w') {
-    drawGrid(radius, width, height);
-    playerPositionY -= 50;
-    checkOutPosition();
-    playerPosition(playerPositionX, playerPositionY);
+    velocityX = 0;
+    velocityY = -1;
   }
   if (event.key === 'ArrowDown' || event.key === 's') {
-    drawGrid(radius, width, height);
-    playerPositionY += 50;
-    checkOutPosition();
-    playerPosition(playerPositionX, playerPositionY);
+    velocityX = 0;
+    velocityY = 1;
   }
   if (event.key === 'ArrowLeft' || event.key === 'a') {
-    drawGrid(radius, width, height);
-    playerPositionX -= 50;
-    checkOutPosition();
-    playerPosition(playerPositionX, playerPositionY);
+    velocityX = -1;
+    velocityY = 0;
   }
   if (event.key === 'ArrowRight' || event.key === 'd') {
-    drawGrid(radius, width, height);
-    playerPositionX += 50;
-    checkOutPosition();
-    playerPosition(playerPositionX, playerPositionY);
+    velocityX = 1;
+    velocityY = 0;
   }
 }
 
-
 // snake motion
-function snakeMotion(){
-    
+function snakeMotion() {
+  playerPositionX += snakeSpeed * velocityX;
+  playerPositionY += snakeSpeed * velocityY;
 }
-
 
 // draw grid
 function drawGrid(radius, width, height) {
@@ -78,13 +76,11 @@ function drawGrid(radius, width, height) {
   }
 }
 
-
 // draw background
 function drawBackground(width, height) {
   ctx.fillStyle = '#fff';
   ctx.fillRect(0, 0, width, height);
 }
-
 
 // player position
 function playerPosition(x, y) {
@@ -95,18 +91,37 @@ function playerPosition(x, y) {
   ctx.fill();
 }
 
-
+// Check if the snake is out the playboard
 function checkOutPosition() {
-  if (playerPositionY > height + radius) {
+  if (playerPositionY > height) {
     playerPositionY = radius;
   }
-  if (playerPositionY < -radius) {
+  if (playerPositionY < 0) {
     playerPositionY = height - radius;
   }
-  if (playerPositionX > width + radius) {
+  if (playerPositionX > width) {
     playerPositionX = radius;
   }
-  if (playerPositionX < -radius) {
+  if (playerPositionX < 0) {
     playerPositionX = width - radius;
   }
 }
+
+// Food
+
+// Give random position of food
+function randomPosition(){
+  let span = width / (2 * radius);
+return Math.floor(Math.random() * span) * (2 * radius) + 25; 
+}
+
+// Give a random number
+function randomNumber() {
+  return Math.floor(Math.random() * 256);
+}
+
+// Give a random color
+function rundomColor(){
+  return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+}
+
