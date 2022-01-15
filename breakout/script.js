0264fb702c15c8f747c4e2062aed2d5caaf55f34
Patrik Duda <patrikduda94@gmail.com>
2022-01-15 10:37:18 +0100

    Create functions for drawing bricks

--- breakout/script.js
+++ breakout/script.js
@@ -31,13 +31,26 @@ const paddle = {
 
 // Brick object
 const brick = {
-    x: 20,
-    y: 20,
-    width: 70,
-    height: 10,
-    padding: 5,
-    offset: 20,
-    visible: true
+  width: 70,
+  height: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+const brickRow = 5;
+const brickColumn = 9;
+
+// Create all bricks
+const bricks = [];
+for (let i = 0; i < brickRow; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumn; j++) {
+    const x = brick.offsetX + (brick.width + brick.padding) * j;
+    const y = brick.offsetY + (brick.height + brick.padding) * i;
+    bricks[i][j] = { x, y, ...brick };
+  }
 }
 
 // CANVAS FUNCTIONS
@@ -83,13 +96,21 @@ function wallCollision() {
 // Paddle reflect
 function checkPaddleReflect() {
   if (
-    ball.x - ball.size > paddle.x &&
-    ball.x + ball.size < paddle.x + paddle.width &&
+    ball.x + ball.size > paddle.x &&
+    ball.x - ball.size < paddle.x + paddle.width &&
     ball.y + ball.size > paddle.y
   ) {
-    ball.dy *= -1; 
+    ball.dy *= -1;
   }
 }
+
+// Game overe 
+function gameOver(){
+  if(ball.y + ball.size > height){
+    location.reload();
+  }
+}
+
 
 // PADDLE FUNCTIONS
 
@@ -129,16 +150,46 @@ function movePaddle() {
   }
 }
 
-
 // BRICK functions
 
-// Draw brick 
-function drawBrick(){
-    ctx.fillStyle = '#f50a54';
-    ctx.fillRect(brick.x, brick.y, brick.width, brick.height);
+// Draw all bricks
+function drawAllBricks() {
+  for (let i = 0; i < bricks.length; i++) {
+    for (let j = 0; j < bricks[i].length; j++) {
+      let brick = bricks[i][j];
+      if (brick.visible) {
+        drawBrick(brick);
+      }
+    }
+  }
 }
 
-drawBrick()
+drawAllBricks();
+
+//  Check if some brick was destroyed
+function brickDestroyed() {
+  for (let i = 0; i < bricks.length; i++) {
+    for (let j = 0; j < bricks[i].length; j++) {
+      let brick = bricks[i][j];
+      if (
+        brick.visible &&
+        brick.x < ball.x - ball.size &&
+        brick.x + brick.width > ball.x + ball.size &&
+        brick.y + brick.height > ball.y - ball.size &&
+        brick.y < ball.y + ball.size
+      ) {
+        ball.dy *= -1;
+        brick.visible = false;
+      }
+    }
+  }
+}
+
+// Draw brick
+function drawBrick(brick) {
+  ctx.fillStyle = '#f50a54';
+  ctx.fillRect(brick.x, brick.y, brick.width, brick.height);
+}
 
 // GAME LOOP
 
@@ -150,6 +201,9 @@ function gameLoop() {
   drawPaddle();
   movePaddle();
   checkPaddleReflect();
+  brickDestroyed();
+  drawAllBricks();
+  gameOver()
 
   requestAnimationFrame(gameLoop);
 }
