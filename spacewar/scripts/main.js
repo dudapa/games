bb909774dc5c504596e5ecab62e980490b4566c5
Patrik Duda <patrikduda94@gmail.com>
2022-02-23 13:11:44 +0100

    Add functions to set a size of the game screen

--- spacewar/scripts/main.js
+++ spacewar/scripts/main.js
@@ -1,5 +1,12 @@
 const canvas = document.querySelector('.canvas');
+canvas.width = widthGameScreen();
+canvas.height = heightGameScreen();
 const ctx = canvas.getContext('2d');
 
+function widthGameScreen() {
+  return window.screen.width - window.screen.width / 2.5;
+}
 
-
+function heightGameScreen() {
+  return window.screen.height - window.screen.height / 4.5;
+}
